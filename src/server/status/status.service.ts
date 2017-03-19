114236e5cc23c93a9b921199156f7d077f531512
Gary Justin <gary.justin@forrent.com>
2017-03-19 16:51:09 -0400

    add built in angular service

--- src/server/status/status.service.ts
+++ src/server/status/status.service.ts
@@ -1,4 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
 export class StatusService {
+
+    constructor(private http: Http) {
+        console.log(http); // proof that Http was provided by angular injector
+    }
+
     public getAppStatus() {
         return {
             status: 'up',
