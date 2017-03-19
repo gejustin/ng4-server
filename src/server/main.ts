a8b0059b0178331025caf8d599a9349955defeab
Gary Justin <gary.justin@forrent.com>
2017-03-19 16:56:53 -0400

    add app failure details

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,6 +4,16 @@ import 'zone.js';
 
 import { ExpressAppModule } from './express-app';
 
+/*
+    Application hangs when you hit http://localhost:4000/
+
+    The rest of the application works as expected. /status endpoint works and correctly utilizes the bootstrapped angular application and DI works.
+*/
 platformDynamicServer().bootstrapModule(ExpressAppModule);
 
+/*
+    Server rendering for http://localhost:4000/ works as expected.
+
+    The rest of the application is not able to utilize angular DI without manually creating an injector, etc..
+*/
 // new ExpressAppModule(null).ngDoBootstrap();
